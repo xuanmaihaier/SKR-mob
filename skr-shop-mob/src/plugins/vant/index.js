8d8033383786209d60692ae3c6497eb109b241e8
z2801810010 <2801810010@qq.com>
2021-04-25 22:20:04 +0800

    sort zjj

--- skr-shop-mob/src/plugins/vant/index.js
+++ skr-shop-mob/src/plugins/vant/index.js
@@ -3,16 +3,16 @@
  * @version: 
  * @Author: stride
  * @Date: 2021-04-20 21:20:37
- * @LastEditors: stride
- * @LastEditTime: 2021-04-25 11:24:22
+ * @LastEditors: zjjj
+ * @LastEditTime: 2021-04-25 21:25:02
  */
 import Vue from 'vue';
 import {
-  Button, NavBar, Icon, Swipe, SwipeItem, Lazyload, Dialog, Tabs, Tab, Cell, Toast, CellGroup, DropdownMenu, Popup, DropdownItem, List, Collapse, CollapseItem, GoodsAction, GoodsActionIcon, GoodsActionButton, ShareSheet, PullRefresh, Card, Form, Field,Stepper
+  Button, NavBar, Icon, Swipe, SwipeItem, Lazyload, Dialog, Tabs, Tab, Cell, Toast, CellGroup, DropdownMenu, Popup, DropdownItem, List, Collapse, CollapseItem, GoodsAction, GoodsActionIcon, GoodsActionButton, ShareSheet, PullRefresh, Card, Form, Field,Stepper,Sidebar,SidebarItem 
 } from "vant"
 // use使用
 const vueUse = {
-  Button, NavBar, Icon, Swipe, SwipeItem, Tabs, Tab, Cell, Dialog, CellGroup, Popup, Toast, List, Collapse, DropdownMenu, DropdownItem, CollapseItem, GoodsAction, GoodsActionIcon, GoodsActionButton, ShareSheet, PullRefresh, Card, Form, Field,Stepper
+  Button, NavBar, Icon, Swipe, SwipeItem, Tabs, Tab, Cell, Dialog, CellGroup, Popup, Toast, List, Collapse, DropdownMenu, DropdownItem, CollapseItem, GoodsAction, GoodsActionIcon, GoodsActionButton, ShareSheet, PullRefresh, Card, Form, Field,Stepper,Sidebar,SidebarItem 
 }
 
 Object.keys(vueUse).forEach(item => {
